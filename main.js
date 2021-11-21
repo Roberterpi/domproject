3a6f73875bd49c39542ba10951c8f71d6d29a9e7
roberterpi <robertpopct@gmail.com>
2021-11-21 17:21:28 +0200

    form submit, delete and filter events

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 var filter = document.getElementById('filter');
+
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+
+
+
+
+
+
+
+
+
